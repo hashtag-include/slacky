b3b8743250209eb0a91800829c281b120f3307bd
Jon Bodner <jonathan.bodner@capitalone.com>
2016-04-13 11:56:42 -0400

    Open link in new window correctly.

--- app/slacky.js
+++ app/slacky.js
@@ -6,53 +6,79 @@ var height = 700;
 
 // Setup the initial Slack window
 chrome.app.runtime.onLaunched.addListener(function() {
-	createWindow(baseUrl);
+    createWindow(baseUrl);
 });
 
 function createWindow(destUrl) {
-	chrome.app.window.create(
-		view, {
-			// Set the height and width and then rander it in the middle of the screen
-			bounds: {
-				width: width,
-				height: height,
-				left: Math.round((screen.availWidth - width) / 2),
-				top: Math.round((screen.availHeight - height) / 2)
-			},
-			// Minimum sizes
-			innerBounds: {
-				minWidth: width,
-				minHeight: height
-			}
-		}, function(createdWindow) {
-			createdWindow.contentWindow.onload = function() {
-				// Retrieve the webview element
-				var webview = createdWindow.contentWindow.document.querySelector('webview');
-				// When the window is resized, resize the webview to conform to the new size
-				createdWindow.onBoundsChanged.addListener(function() {
-					var bounds = createdWindow.getBounds();
-					webview.style.height = bounds.height;
-					webview.style.width = bounds.width;
-				});
-				// Render the page requested inside the webview
-				webview.src = destUrl;
-				// Add event listener for when webview finishes loading
-				webview.addEventListener("contentload", function() {
-					// Focus on the webview, so focus can go to the proper elements within it
-					webview.focus();
-					// Add event listener for when window becomes focused
-					createdWindow.contentWindow.onfocus = function() {
-						// Focus on the webview, so focus can go to the proper elements within it
-						webview.focus();
-					};
-				});
-				webview.addEventListener('newwindow', function(event) {
-					// Event handler for when external links are clicked because of
-					// the Chrome packaged app security restriction on opening links in the regular browser
-					event.preventDefault();
-					window.open(event.targetUrl);
-				});
-			};
-		}
-	);
+    chrome.app.window.create(
+        view, {
+            // Set the height and width and then rander it in the middle of the screen
+            bounds: {
+                width: width,
+                height: height,
+                left: Math.round((screen.availWidth - width) / 2),
+                top: Math.round((screen.availHeight - height) / 2)
+            },
+            // Minimum sizes
+            innerBounds: {
+                minWidth: width,
+                minHeight: height
+            }
+        },
+        function(createdWindow) {
+            createdWindow.contentWindow.onload = function() {
+                // Retrieve the webview element
+                var webview = createdWindow.contentWindow.document.querySelector('webview');
+                // When the window is resized, resize the webview to conform to the new size
+                createdWindow.onBoundsChanged.addListener(function() {
+                    var bounds = createdWindow.getBounds();
+                    webview.style.height = bounds.height;
+                    webview.style.width = bounds.width;
+                });
+                // Render the page requested inside the webview
+                webview.src = destUrl;
+                // Add event listener for when webview finishes loading
+                webview.addEventListener("contentload", function() {
+                    // Focus on the webview, so focus can go to the proper elements within it
+                    webview.focus();
+                    // Add event listener for when window becomes focused
+                    createdWindow.contentWindow.onfocus = function() {
+                        // Focus on the webview, so focus can go to the proper elements within it
+                        webview.focus();
+                    };
+                });
+                webview.addEventListener('newwindow', function(event) {
+                        // Event handler for when external links are clicked because of
+                        // the Chrome packaged app security restriction on opening links in the regular browser
+                        event.preventDefault();
+												var url = event.targetUrl;
+                        chrome.app.window.create(
+                            view, {
+                                hidden: true,
+																innerBounds: {
+																	width:1024,
+																	height: 768
+																}
+                            }, // only show window when webview is configured
+                            function(appWin) {
+                                appWin.contentWindow.addEventListener('DOMContentLoaded',
+                                    function(e) {
+                                        // when window is loaded, set webview source
+                                        var webview = appWin.contentWindow.document.querySelector('webview');
+																				appWin.onBoundsChanged.addListener(function() {
+												                    var bounds = appWin.getBounds();
+												                    webview.style.height = bounds.height;
+												                    webview.style.width = bounds.width;
+												                });
+                                        webview.src = url;
+                                        // now we can show it:
+                                        appWin.show();
+                                    }
+                                );
+                            });
+                    }
+                );
+        };
+    }
+);
 }
